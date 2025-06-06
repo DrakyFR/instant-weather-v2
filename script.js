573470496509a036869fec6f15a686989cb09ed3
drakyfr <leosadoev@gmail.com>
2025-06-06 17:24:31 +0200

    6 commit

--- script.js
+++ script.js
@@ -8,3 +8,23 @@ const basculeurTheme = document.getElementById('basculeur-theme');
 let joursSelectionnes = 3;
 const TOKEN_API = '55febe6c0af763a8c85a5720fb02e11fa81558ada191c2faee729f4f3d3bfbda';
 
+// loader 
+window.addEventListener('load', function() {
+    
+    setTimeout(() => {
+        const loaderInitial = document.getElementById('loader-initial');
+        const contenuPrincipal = document.getElementById('contenu-principal');
+        
+        // Masquer le loader
+        loaderInitial.classList.add('hidden');
+        
+    
+        setTimeout(() => {
+            contenuPrincipal.classList.add('visible');
+        
+            setTimeout(() => {
+                loaderInitial.remove();
+            }, 500);
+        }, 250);
+    }, 2000); // 2 secondes d'affichage du loader
+});
